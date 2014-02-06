4e9349773e6b865ceb75d27a912028e6238230df
dijk <dijkyon@gmail.com>
2014-02-06 06:56:47 -0800

    add!

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("It is chilly out there!");
+		System.out.println(add(2,3));
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
 	}
 
 }
