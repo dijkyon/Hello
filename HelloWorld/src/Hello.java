77845c22060c1ad147348b34929e24feabd92ad1
dijk <dijkyon@gmail.com>
2014-02-08 05:28:14 -0800

    sub

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,10 +4,15 @@ public class Hello {
 		System.out.println("hello world!");
 		System.out.println("It is chilly out there!");
 		System.out.println(add(2,3));
+		System.out.println(sub(3,2));
 	}
 	
 	public static int add(int a, int b) {
 		return a+b;
 	}
+	public static int sub(int a, int b) {
+		return a-b;
+	}
+
 
 }
